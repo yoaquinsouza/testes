dc53d612994d69aee17ccb2c2a7826586c48a569
yoaquinsouza <yoaquinsouza@gmail.com>
2025-11-02 18:06:58 -0300

    Update [slug].js

--- api/pv/[slug].js
+++ api/pv/[slug].js
@@ -1,19 +1,27 @@
 export default function handler(req, res) {
   const { slug } = req.query;
-  const all = [...require('../../todos.json'), ...require('../../masculino.json'), ...require('../../feminino.json')];
+  const all = [
+    ...require('../../todos.json'),
+    ...require('../../masculino.json'),
+    ...require('../../feminino.json')
+  ];
+
   const produto = all.find(p => p.imagem.toLowerCase().includes(slug));
   if (!produto) return res.status(404).send('Produto não encontrado');
 
-  const domain = `https://${req.headers.host}`;
+  const DOMAIN = "https://testes-phi-five.vercel.app";
+
   const html = `
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
 <meta charset="UTF-8">
+
 <meta property="og:title" content="${produto.modelo}">
 <meta property="og:description" content="Tamanhos: ${produto.tamanhos}">
-<meta property="og:image" content="https://testes-phi-five.vercel.app/${produto.imagem}">
+<meta property="og:image" content="${DOMAIN}${produto.imagem}">
 <meta property="og:type" content="website">
+
 <title>${produto.modelo}</title>
 
 <style>
@@ -40,13 +48,17 @@ export default function handler(req, res) {
   }
 </style>
 
-
 </head>
 <body>
-<img src="https://testes-phi-five.vercel.app/${produto.imagem}" alt="${produto.modelo}" 
+
+<h2>${produto.modelo}</h2>
+<p>Tamanhos: ${produto.tamanhos}</p>
+<img class="imgProduto" src="${DOMAIN}${produto.imagem}" alt="${produto.modelo}"/>
 
 </body>
-</html>`;
+</html>
+  `;
+
   res.setHeader('Content-Type', 'text/html');
   res.send(html);
 }
